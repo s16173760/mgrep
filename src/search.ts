98e915ab181065339e2ff949ffbfbbe3aa619884
Joel Dierkes <joel@mixedbread.com>
2025-11-10 22:54:03 -0800

    chore: display the filepath and line numbers

--- src/search.ts
+++ src/search.ts
@@ -1,9 +1,29 @@
+import type {
+  ScoredAudioURLInputChunk,
+  ScoredImageURLInputChunk,
+  ScoredTextInputChunk,
+  ScoredVideoURLInputChunk,
+} from "@mixedbread/sdk/resources/vector-stores/vector-stores";
 import type { Command } from "commander";
 import { Command as CommanderCommand } from "commander";
 import { getJWTToken } from "./lib/auth";
 import { createMxbaiClient } from "./lib/mxbai";
 import type { FileMetadata } from "./types";
 import { ensureAuthenticated } from "./utils";
+
+type ChunkType =
+  | ScoredTextInputChunk
+  | ScoredImageURLInputChunk
+  | ScoredAudioURLInputChunk
+  | ScoredVideoURLInputChunk;
+
+function formatChunk(chunk: ChunkType) {
+  const path = (chunk.metadata as FileMetadata)?.path ?? "Unknown path";
+  const line_range = chunk.generated_metadata?.start_line
+    ? `:${chunk.generated_metadata?.start_line}:${(chunk.generated_metadata?.start_line as number) + (chunk.generated_metadata?.num_lines as number)}`
+    : "";
+  return `${path}${line_range}`;
+}
 
 export const search: Command = new CommanderCommand("search")
   .description("File pattern searcher")
@@ -33,18 +53,7 @@ export const search: Command = new CommanderCommand("search")
         },
       });
 
-      console.log(
-        results.data
-          .map((result) => {
-            let content =
-              result.type === "text"
-                ? result.text
-                : `Not a text chunk! (${result.type})`;
-            content = JSON.stringify(content);
-            return `${(result.metadata as FileMetadata)?.path ?? "Unknown path"}: ${content}`;
-          })
-          .join("\n"),
-      );
+      console.log(results.data.map(formatChunk).join("\n"));
     } catch (error) {
       const message = error instanceof Error ? error.message : "Unknown error";
       console.error("Failed to search:", message);
